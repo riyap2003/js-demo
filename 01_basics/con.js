8b01ed2295c8c0d6a51a9d2b7411b43e442b1d3f
riyap2003 <159626756+riyap2003@users.noreply.github.com>
2024-12-06 07:20:45 +0000

    conversion of datatype

---
+++ 01_basics/con.js
@@ -0,0 +1,17 @@
+//let score=33;
+//let score="33abc";
+//let score=null;
+// let score=undefined;
+let score=true;
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);//number
+console.log(valueInNumber);//NaN
+//number,...=>Boolean
+//1=>true 0=>false
+//""=>false "ria"=>true
+//String(),Number(),Boolean()
