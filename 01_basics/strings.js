b0514f206ed041a0ecde6d2bc12e56a369024216
riyap2003 <159626756+riyap2003@users.noreply.github.com>
2024-12-06 11:57:16 +0000

    String basic

---
+++ 01_basics/strings.js
@@ -0,0 +1,45 @@
+// const name="riya"
+// const repoCount=50
+// // console.log(name+repoCount+" value Count")
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+
+const gameName= new String('riya-p-billava')
+
+//console.log(gameName);
+
+// console.log(gameName[0]);//r
+// console.log(gameName.__proto__);//{}
+// console.log(gameName.length);//4
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));//a
+// console.log(gameName.charCodeAt('y'));//114
+// console.log(gameName.indexOf('y'));//2
+
+// const newString=gameName.substring(0,4)
+// console.log(newString);
+
+
+// const anotherString=gameName.slice(-10,6)
+// console.log(anotherString);
+
+
+// const newStringOne="    riyap    "
+
+// console.log(newStringOne.trim());//trimStart(),trimEnd()
+
+
+const url="https://riya.com/riya%10billava"
+console.log(url.replace("%10","_"))
+console.log(url)
+console.log(url.includes('riya'))
+
+console.log(gameName.split('-'))
+
+
+
+
+
+
+
+
