9df56fe493f80fc3eb83b0780e3533296d9953bb
riyap2003 <159626756+riyap2003@users.noreply.github.com>
2024-12-06 07:02:07 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict";//treat all js code as newer version
+
+// alert("hello"); //we are using node.js,not browser
+
+console.log(3+3) 
+console.log("riya")
+
+let name="riya"
+let age=33
+let LoggedIn=true
+
+//  Primitive data types
+// number =>2 to power 53
+// bigint 
+// String => " " ' '
+// Boolean => true false
+// null =>Standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof "hi")
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
+
