c7619ea318ffe022630acd5072cc1888cf81393a
riyap2003 <159626756+riyap2003@users.noreply.github.com>
2024-12-06 10:46:54 +0000

    operations

---
+++ 01_basics/conop.js
@@ -0,0 +1,48 @@
+/*********************Operations***********************/
+// let value=3
+// let negValue=-value
+
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+// let str1="riya"
+// let str2="hello"
+// let str3=str1+" "+str2
+// console.log(str3)
+
+// console.log("1"+2)//12
+// console.log(1+"2")//12
+// console.log("1"+2+2)//122
+// console.log(1+1+"2")//22
+
+// console.log(true)//true
+
+// console.log(+true)//1
+// console.log(+" ")//0
+
+// let num1,num2,num3
+
+// num1=num2=num3=2+2
+
+let gameCounter=100
+//gameCounter++;//101
+++gameCounter;//101
+console.log(gameCounter);
+
+//console.log(++gameCounter);//102
+
+console.log(gameCounter++);//101
+
+
+
+
+
+
+
+
