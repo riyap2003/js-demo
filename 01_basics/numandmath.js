b7fac169af92a5c6dc93601605d8f5c09ca62628
riyap2003 <159626756+riyap2003@users.noreply.github.com>
2024-12-12 09:54:25 +0000

    num

---
+++ 01_basics/numandmath.js
@@ -0,0 +1,17 @@
+const balance=new Number(200)
+console.log(balance)
+
+const score=212
+console.log(score)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2))
+
+
+const otherNumber = 23.999
+
+console.log(otherNumber.toPrecision(5))
+
+
+const hunderds=1000000
+console.log(hunderds.toLocaleString('en-IN'))
